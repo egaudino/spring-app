0b384ec360d86fde27162132e800e1074a0b289a
Eduardo Gaudino <eduardo.gaudino@gmail.com>
2017-09-01 00:25:15 -0300

    Create README.me
    Readme file

---
+++ README.d
@@ -0,0 +1,13 @@
+# Spring-app Article
+
+RESTful CRUD Application 
+
+# Usage
+
+- Clone repository
+- Set Mysql Database
+- run: mvn spring-boot:run
+
+# Frameworks
+Spring Web, JAX-RS, JPA/Hibernate
+
