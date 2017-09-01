2550844e1f43ed2e0e1b006da666709e32df924f
Eduardo Gaudino <eduardo.gaudino@gmail.com>
2017-09-01 00:29:37 -0300

    Update README.d

--- README.d
+++ README.d
@@ -2,11 +2,28 @@
 
 RESTful CRUD Application 
 
-# Usage
+# Setup
 
 - Clone repository
 - Set Mysql Database
 - run: mvn spring-boot:run
+
+# Usage
+
+- Get all articles:
+http://localhost:8080/spring-app/article/details
+
+- Get one article:
+http://localhost:8080/spring-app/article/{id}
+
+- Add article:
+http://localhost:8080/spring-app/article/add
+
+- Edit Article
+http://localhost:8080/spring-app/article/update
+
+- Delete Article
+http://localhost:8080/spring-app/article/delete/{id}
 
 # Frameworks
 Spring MVC, JAX-RS, JPA/Hibernate
